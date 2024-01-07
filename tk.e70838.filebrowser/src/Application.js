2c405c208ce7d702eb9149e015f1ff874702283b
Jean-François Bocquet <pulpficti@gmail.com>
2024-01-07 18:41:19 +0100

    Chapter II.4 UI Templates and Composite Widgets

--- tk.e70838.filebrowser/src/Application.js
+++ tk.e70838.filebrowser/src/Application.js
@@ -1,40 +1,14 @@
 import Gtk from 'gi://Gtk';
 import GObject from 'gi://GObject';
 
+import './WelcomeWidget.js';
+import { Window } from './Window.js';
+
 export const FbrApplication = GObject.registerClass({
 	GTypeName: 'FbrApplication'
 }, class extends Gtk.Application {
     vfunc_activate() {
-		const window = new Gtk.ApplicationWindow({ application: this });
-	    const box = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL,
-            marginTop: 36,
-            marginBottom: 36,
-            marginStart: 36,
-            marginEnd: 36,
-            spacing: 18,
-            valign: Gtk.Align.CENTER
-        });
-
-        // flatpak run org.gnome.design.IconLibrary
-        const image = new Gtk.Image({
-            iconName: 'system-file-manager-symbolic',
-            iconSize: Gtk.IconSize.LARGE
-        });
-        box.append(image);
-
-        const label = new Gtk.Label({
-            label: 'Welcome to our new file browser!',
-            wrap: true
-        });
-        box.append(label);
-
-        const button = new Gtk.Button({
-            label: 'Let\'s go!',
-            halign: Gtk.Align.CENTER
-        });
-        box.append(button);
-
-        window.child = box;
+		const window = new Window({ application: this });
 	    window.present();
 	}
 });
