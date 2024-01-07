0962d2fc7d9d715713e1347fad35dca9434f60f2
Jean-François Bocquet <pulpficti@gmail.com>
2024-01-07 18:41:19 +0100

    Chapter II.3 Commonly Used Widgets

--- tk.e70838.filebrowser/src/Application.js
+++ tk.e70838.filebrowser/src/Application.js
@@ -6,8 +6,35 @@ export const FbrApplication = GObject.registerClass({
 }, class extends Gtk.Application {
     vfunc_activate() {
 		const window = new Gtk.ApplicationWindow({ application: this });
-	    const label = new Gtk.Label({ label: 'Hello World!' });
-	    window.child = label;
+	    const box = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL,
+            marginTop: 36,
+            marginBottom: 36,
+            marginStart: 36,
+            marginEnd: 36,
+            spacing: 18,
+            valign: Gtk.Align.CENTER
+        });
+
+        // flatpak run org.gnome.design.IconLibrary
+        const image = new Gtk.Image({
+            iconName: 'system-file-manager-symbolic',
+            iconSize: Gtk.IconSize.LARGE
+        });
+        box.append(image);
+
+        const label = new Gtk.Label({
+            label: 'Welcome to our new file browser!',
+            wrap: true
+        });
+        box.append(label);
+
+        const button = new Gtk.Button({
+            label: 'Let\'s go!',
+            halign: Gtk.Align.CENTER
+        });
+        box.append(button);
+
+        window.child = box;
 	    window.present();
 	}
 });
